32ef6f84b9beae8b85ac376a52a585648fa93983
Fawkward <orlenokkforever@gmail.com>
2023-02-26 15:58:11 +0100

    Fix: Some new fixes

--- src/server/src/deletecars.ts
+++ src/server/src/deletecars.ts
@@ -1,21 +1,20 @@
 mp.events.add(
 	'C2S:keyDown:delCars',
 	(player: PlayerMp, position: Vector3Mp, radius: number) => {
-		let mad1 = forcars(player.position, 100);
-		//console.log(mad1);
-		mad1.forEach((x: VehicleMp) => {
+		let vehList = forCars(player.position, 100);
+		vehList.forEach((x: VehicleMp) => {
 			x.destroy();
 		});
 		console.log(
-			`[SERVER] Удалены все машины в радиусе ${radius} метров от игрока ${player.name}.`
+			`[SERVER] Удалены все машины в раsдиусе ${radius} метров от игрока ${player.name}.`
 		);
 	}
 );
 
-function forcars(position: Vector3Mp, radius: number): VehicleMp[] {
-	let mand: VehicleMp[] = [];
+function forCars(position: Vector3Mp, radius: number): VehicleMp[] {
+	let vehList: VehicleMp[] = [];
 	mp.vehicles.forEachInRange(position, radius, (vehicle: VehicleMp) => {
-		mand.push(vehicle);
+		vehList.push(vehicle);
 	});
-	return mand;
+	return vehList;
 }
